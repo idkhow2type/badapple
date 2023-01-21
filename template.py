35de036e5958eeef0820c6159168e1606c9dd5f7
idkhow2type <lecaominhhn@gmail.com>
2023-01-22 02:55:07 +0700

    fix minor template thing

--- template.py
+++ template.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-for i, file in enumerate(os.listdir(f"frames")):
+for i, file in enumerate(os.listdir("frames")):
     file = f"frames/{file}"
     image = Image.open(file)
     image = image.convert("L").point(lambda x: 255 if x > 127 else 0, mode="1")
