0b1b48e15b28ba43f751be8e9df108ae6c98b601
idkhow2type <lecaominhhn@gmail.com>
2023-01-22 16:01:53 +0700

    fix mp3thumb

---
+++ mp3thumb/README.md
@@ -0,0 +1,3 @@
+# Bad Apple on mp3
+
+Run `main.py`, then get a mp3 player that displays thumbnails and moves to the next "song" automatically to watch this
--- mp3thumb/main.py
+++ mp3thumb/main.py
@@ -1,75 +1,49 @@
 import os
-from root.utils import to_frame, printProgressBar
 from PIL import Image
 from pydub import AudioSegment
 import shutil
 from mutagen.mp3 import MP3
 from mutagen.id3 import APIC
+import json
 
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+with open("config.json", "r") as f:
+    global CONFIG
+    CONFIG = json.load(f)
 
-CONFIG = {
-    "width": 48,
-    "height": 36,
-    "fps": 5,
-}
-
-to_frame(CONFIG["width"], CONFIG["height"], CONFIG["fps"], ROOT_PATH)
-
-path = f"{ROOT_PATH}/result"
-if os.path.exists(path):
-    shutil.rmtree(path)
-os.mkdir(path)
+if os.path.exists("result"):
+    shutil.rmtree("result")
+os.mkdir("result")
 
 # TODO: implement multiprocessing
-i = 0
-printProgressBar(
-    i,
-    len(os.listdir(f"{ROOT_PATH}/frames")),
-    prefix="Progress",
-    suffix="Complete",
-    length=50,
-)
-file_count = len(os.listdir(f"{ROOT_PATH}/frames"))
-for file in os.listdir(f"{ROOT_PATH}/frames"):
+file_count = len(os.listdir("frames"))
+for i,file in enumerate(os.listdir(f"frames")):
     file_name = os.path.splitext(file)[0]
 
     # add border to make image square
-    img = Image.open(f"{ROOT_PATH}/frames/{file}")
+    img = Image.open(f"frames/{file}")
     size = max(CONFIG["width"], CONFIG["height"])
     size = (size, size)
     new_img = Image.new("RGB", size, color="black")
     new_img.paste(
         img, (int((size[0] - img.size[0]) / 2), int((size[1] - img.size[1]) / 2))
     )
-    new_img.save(f"{ROOT_PATH}/frames/{file}")
+    new_img.save(f"frames/{file}")
 
     # create silent mp3 file
     # TODO: replace with segment of music to each mp3 file
     AudioSegment.silent(duration=1000 / (CONFIG["fps"] * 2)).export(
-        f"{ROOT_PATH}/result/{file_name}.mp3", format="mp3"
+        f"result/{file_name}.mp3", format="mp3"
     )
     # add id3 tag to mp3 file
-    audio = MP3(f"{ROOT_PATH}/result/{file_name}.mp3")
+    audio = MP3(f"result/{file_name}.mp3")
     audio.tags.add(
         APIC(
             encoding=3,
             mime="image/png",
             type=3,
             desc="Cover",
-            data=open(f"{ROOT_PATH}/frames/{file}", "rb").read(),
+            data=open(f"frames/{file}", "rb").read(),
         )
     )
     audio.save()
-    i += 1
-    printProgressBar(
-        i,
-        file_count,
-        prefix="Progress",
-        suffix="Complete",
-        decimals=2,
-        length=80,
-    )
-
-# remove frames folder
-shutil.rmtree(f"{ROOT_PATH}/frames")
+    print(f'Processed {i}/{file_count}',end='\r')
